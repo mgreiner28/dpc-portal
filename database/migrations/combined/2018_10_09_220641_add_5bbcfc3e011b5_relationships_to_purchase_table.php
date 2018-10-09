d195c8e812f471dbf360912956e7bfb83d48261f
Michael <mi42291@C02RJC9SG8WN.local>
2018-10-09 15:33:26 -0400

    further file changes

---
+++ database/migrations/combined/2018_10_09_220641_add_5bbcfc3e011b5_relationships_to_purchase_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Add5bbcfc3e011b5RelationshipsToPurchaseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('purchases', function(Blueprint $table) {
+            if (!Schema::hasColumn('purchases', 'assigned_to_id')) {
+                $table->integer('assigned_to_id')->unsigned()->nullable();
+                $table->foreign('assigned_to_id', '216308_5bba186224936')->references('id')->on('users')->onDelete('cascade');
+                }
+                if (!Schema::hasColumn('purchases', 'created_by_id')) {
+                $table->integer('created_by_id')->unsigned()->nullable();
+                $table->foreign('created_by_id', '216308_5bba1823e38e3')->references('id')->on('users')->onDelete('cascade');
+                }
+                
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('purchases', function(Blueprint $table) {
+            if(Schema::hasColumn('purchases', 'assigned_to_id')) {
+                $table->dropForeign('216308_5bba186224936');
+                $table->dropIndex('216308_5bba186224936');
+                $table->dropColumn('assigned_to_id');
+            }
+            if(Schema::hasColumn('purchases', 'created_by_id')) {
+                $table->dropForeign('216308_5bba1823e38e3');
+                $table->dropIndex('216308_5bba1823e38e3');
+                $table->dropColumn('created_by_id');
+            }
+            
+        });
+    }
+}
